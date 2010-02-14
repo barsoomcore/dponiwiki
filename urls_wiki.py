d686e7355919f38dc02bb50e1eb1d390196efae3
barsoomcore <coreyr@mac.com>
2010-02-14 13:42:32 -0500

    Some proper password reset functionality.

--- urls_wiki.py
+++ urls_wiki.py
@@ -54,11 +54,32 @@ urlpatterns += patterns('django.contrib.auth.views',
 	url(r'^accounts/password_change/$', 
 		'password_change', 
 		{'template_name': 'templates/registration/password_change_form.html'}, 
-		name='password_reset'
+		name='password_change'
 	),
 	url(r'^accounts/password_change/done/$', 
 		'password_change_done', 
 		{'template_name': 'templates/registration/password_change_done.html'}
+	),
+	url(r'^accounts/password_reset/$',
+		'password_reset',
+		{'template_name': 'templates/registration/password_reset_form.html',
+		'email_template_name': 'templates/registration/password_reset_email.html'},
+		name='password_reset'
+	),
+	url(r'^accounts/password_reset/done/$',
+		'password_reset_done',
+		{'template_name': 'templates/registration/password_reset_done.html'},
+		name='password_reset_done'
+	),
+	url(r'^accounts/password_reset_confirm/(?P<uidb36>[0-9A-Za-z]+)-(?P<token>.+)/$',
+		'password_reset_confirm',
+		{'template_name': 'templates/registration/password_reset_confirm.html'},
+		name='password_reset_confirm'
+	),
+	url(r'^accounts/password_reset_complete/$',
+		'password_reset_complete',
+		{'template_name': 'templates/registration/password_reset_complete.html'},
+		name='password_reset_complete'
 	),
 )
 
