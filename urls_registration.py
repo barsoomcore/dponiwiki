2cc5a49c8e29318e2c3751669331a9d513f44a2e
barsoomcore <coreyr@mac.com>
2010-02-20 19:26:57 -0500

    Fix login links.

--- urls_registration.py
+++ urls_registration.py
@@ -2,42 +2,42 @@ from django.conf.urls.defaults import *
 from django.contrib.auth.views import login
 
 urlpatterns = patterns('django.contrib.auth.views',
-    url(r'^accounts/login/$',  
+    url(r'^login/$',  
     	'login', 
     	{ 'template_name': 'templates/registration/login.html'}, 
     	name='login'
     ),
-    url(r'^accounts/logout/$', 
+    url(r'^logout/$', 
     	'logout', 
     	{'next_page': '/dponiwiki/'}, 
     	name='logout'
     ),
-	url(r'^accounts/password_change/$', 
+	url(r'^password_change/$', 
 		'password_change', 
 		{'template_name': 'templates/registration/password_change_form.html'}, 
 		name='password_change'
 	),
-	url(r'^accounts/password_change/done/$', 
+	url(r'^password_change/done/$', 
 		'password_change_done', 
 		{'template_name': 'templates/registration/password_change_done.html'}
 	),
-	url(r'^accounts/password_reset/$',
+	url(r'^password_reset/$',
 		'password_reset',
 		{'template_name': 'templates/registration/password_reset_form.html',
 		'email_template_name': 'templates/registration/password_reset_email.html'},
 		name='password_reset'
 	),
-	url(r'^accounts/password_reset/done/$',
+	url(r'^password_reset/done/$',
 		'password_reset_done',
 		{'template_name': 'templates/registration/password_reset_done.html'},
 		name='password_reset_done'
 	),
-	url(r'^accounts/password_reset_confirm/(?P<uidb36>[0-9A-Za-z]+)-(?P<token>.+)/$',
+	url(r'^password_reset_confirm/(?P<uidb36>[0-9A-Za-z]+)-(?P<token>.+)/$',
 		'password_reset_confirm',
 		{'template_name': 'templates/registration/password_reset_confirm.html'},
 		name='password_reset_confirm'
 	),
-	url(r'^accounts/password_reset_complete/$',
+	url(r'^password_reset_complete/$',
 		'password_reset_complete',
 		{'template_name': 'templates/registration/password_reset_complete.html'},
 		name='password_reset_complete'
