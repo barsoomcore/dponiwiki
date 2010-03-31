a55aedc843c75566ff1f8435b63acbd90238c27e
barsoomcore <coreyr@mac.com>
2010-03-30 23:59:59 -0400

    Inter-wiki links made easy!

--- templatetags/taggingextras.py
+++ templatetags/taggingextras.py
@@ -1,4 +1,8 @@
+import re
 from django import template
+from markup import textile
+
+from dponisetting.dponiwiki.models import Island, IslandComponent
 
 register = template.Library()
 
@@ -15,3 +19,22 @@ def matches(list1, list2):
 		return True
 	else:
 		return False
+
+def find_item(match):
+	try:
+		item = Island.objects.get(name__exact=match.group('name'))
+	except Island.DoesNotExist:
+		try:
+			item = IslandComponent.objects.get(name__exact=match.group('name'))
+		except IslandComponent.DoesNotExist:
+			return match.group()
+	
+	return "<a href=\"" + item.get_absolute_url() + "\">" + match.group('name') + "</a>"
+
+
+@register.filter
+def dinostyle(content):
+	pattern = re.compile(r'\[\[(?P<name>[^\]\]]*)\]\]')
+	linked_content = pattern.sub(find_item, content)
+	
+	return textile(linked_content)
